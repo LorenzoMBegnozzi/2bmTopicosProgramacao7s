55feb906a36f39dc0bca18d34a00fe5b9b4442e9
LorenzoBerg <lorenzobegnozzi@hotmail.com>
2025-05-07 19:53:53 -0300

    Inicializando o projeto com as dependencias básicas

---
+++ src/test/java/com/example/Gerenciador_de_Tarefa/GerenciadorDeTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gerenciador_de_Tarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
